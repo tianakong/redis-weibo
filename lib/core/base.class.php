8ed65abf66d11b48d83ab2f889d05066d4760526
tiankong <67063492@qq.com>
2015-09-21 16:04:30 +0800

    view 增加layout

--- lib/core/base.class.php
+++ lib/core/base.class.php
@@ -3,13 +3,28 @@
 class base
 {
     /**
+     * 公共文件
+     */
+    private function layout($content)
+    {
+        $view = fendou_lib.'app/'.$GLOBALS['m'] .'/view/'.'layout/layout.php';
+        if(file_exists($view)) {
+            include $view;
+            exit;
+        } else {
+            exit('layout.php 不存在');
+        }
+    }
+
+    /**
      * 渲染视图
      */
     public function display()
     {
         $view = fendou_lib.'app/'.$GLOBALS['m'] .'/view/'.$GLOBALS['c'] .'/'.$GLOBALS['a'].VIEW_SUFFIX;
         if(file_exists($view)) {
-            include $view;
+            $content = include $view;
+            $this->layout($content);
             exit;
         } else {
             exit('模版不存在');
